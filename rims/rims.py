24c18a0b4c0b9c810d456831573465fa1520d704
tsujl <180871193+tsujl@users.noreply.github.com>
2025-02-05 01:02:38 +0900

    rims hist

--- rims/rims.py
+++ rims/rims.py
@@ -14,7 +14,7 @@ bins = np.arange(1.490, 1.510+bin_width, bin_width)
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.hist(data, bins=bins, histtype="bar", ec="black", color="blue")
+ax.hist(data, bins=bins, histtype="bar", edgecolor="black", color="blue")
 
 #軸の設定
 plt.xlabel("Refractive index (nd)")
@@ -23,6 +23,6 @@ ax.set_ylim(0, 30)
 xticks = np.arange(1.490, 1.510, 0.005)
 plt.xticks(xticks, [f"{x:.3f}" for x in xticks])
 
-
+plt.savefig("SK_ash.png")
 plt.show()
 # %%
